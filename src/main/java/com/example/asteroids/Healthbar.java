6f7759c3e2229981fe4efe25f704b07a49f17ea1
tirox67 <166658856+tirox67@users.noreply.github.com>
2025-03-13 23:18:47 +0100

    Added a healthbar

---
+++ src/main/java/com/example/asteroids/Healthbar.java
@@ -0,0 +1,40 @@
+package com.example.asteroids;
+
+public class Healthbar {
+
+    private double coordX;
+    private double coordY;
+    private final double maxLength = 200;
+    private Player player;
+
+
+    public Healthbar(double coordX, double coordY) {
+
+        this.coordX = coordX;
+        this.coordY = coordY;
+
+    }//end of constructor
+
+    public void update() {
+        this.coordX = player.getCoordX();
+        this.coordY = player.getCoordY();
+    }//end of update
+
+    public void attachPlayer(Player player) {
+        this.player = player;
+    }//end of attachPlayer
+
+    //getters
+    public double getCoordX() {return coordX;}
+
+    public double getCoordY() {return coordY;}
+
+    public double getMaxLength() {return maxLength;}
+
+    //setters
+    public void setCoordX(double coordX) {this.coordX = coordX;}
+
+    public void setCoordY(double coordY) {this.coordY = coordY;}
+
+
+}//end of Healthbar
