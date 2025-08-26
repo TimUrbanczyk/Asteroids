878b44d4d2df0ab1b1de14126c56594da6df3932
tirox67 <166658856+tirox67@users.noreply.github.com>
2025-08-27 01:02:31 +0200

    chore: add skeleton logic to Infernoid for movement

--- src/main/java/com/example/asteroids/BossFights/Bosses/Infernoid.java
+++ src/main/java/com/example/asteroids/BossFights/Bosses/Infernoid.java
@@ -5,22 +5,59 @@ import com.example.asteroids.Asteroids.Asteroid;
 
 import java.util.Random;
 
-//eed one instance for the boss, therefore use singletoen here
 public class Infernoid extends Asteroid {
 
-
-    //singleton instance
     private static Infernoid infernoid;
-    //fields
     private static final double speedX =2.7 ;
     private static final double speedY = 2.7;
     private static final int damagePoints = 100;
     private static final String name = "Infernoid";
+    private BossState currentState;
 
 
     private Infernoid(){
         super(speedX,speedY,"",damagePoints);
+        this.currentState = BossState.WANDER;
     }
+
+    public enum BossState {
+        WANDER,
+        SUMMOM,
+        SPLIT,
+        DIE
+    }
+
+    public void changeState(BossState newState){
+        this.currentState = newState;
+    }
+
+    public void move(){
+        switch (currentState){
+            case WANDER -> {}
+            case SUMMOM -> {}
+            case SPLIT -> {}
+            case DIE -> {}
+        }
+    }
+
+
+    private void moveWander(){
+        //TODO
+    }
+
+    private void moveSummon(){
+        //TODO
+    }
+
+    private void moveSplit(){
+        //TODO
+    }
+
+    private void moveDie(){
+        //TODO
+    }
+
+
 
     public static Infernoid getInfernoid(){
         if(infernoid == null){
@@ -31,21 +68,8 @@ public class Infernoid extends Asteroid {
     }
 
 
-    public double[] calculateNewCoords(double oldCoordX, double oldCoordY){
 
 
-        Random rand = new Random();
-
-        double newCoordX = rand.nextDouble()+1;
-        double newCoordY = rand.nextDouble()+1;
-
-        double[] output = new double[2];
-        output[0] = newCoordX;
-        output[1] = newCoordY;
-        return output;
-
-
-    }
 
 
 }
