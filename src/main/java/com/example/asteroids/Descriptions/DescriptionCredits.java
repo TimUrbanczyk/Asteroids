d7a0829d233c8a974836d25ca422c4a32a6b5cdd
tirox67 <166658856+tirox67@users.noreply.github.com>
2025-09-16 14:22:39 +0200

    feat: implement credits

---
+++ src/main/java/com/example/asteroids/Descriptions/DescriptionCredits.java
@@ -0,0 +1,15 @@
+package com.example.asteroids.Descriptions;
+
+public class DescriptionCredits implements DescriptionInterface{
+
+    private static final String descriptionFastSneak = "Music cooked by @Fastsneak";
+    private static final String descriptionTirox67 = "Logik cooked by @tirox67";
+
+    public static String getDescriptionFastSneak() {
+        return descriptionFastSneak;
+    }
+
+    public static String getDescriptionTirox67() {
+        return descriptionTirox67;
+    }
+}
